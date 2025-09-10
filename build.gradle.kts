5fa6c186012fe95421da16e26bd6117f5f7a118b
Tyler Fibonacci <gepheum@gmail.com>
2025-09-10 18:34:14 +0200

    upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ mavenPublishing {
     coordinates(
         groupId = "land.soia",
         artifactId = "soia-kotlin-client",
-        version = "1.0.0",
+        version = "1.0.7",
     )
 
     // Configure POM metadata for the published artifact
